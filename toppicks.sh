e689cb0a22f717d996785c025d4934ea302b72f8
danmed <danmed@gmail.com>
2016-09-13 08:31:09 +0100

    Update toppicks.sh

--- toppicks.sh
+++ toppicks.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-wget http://toppicks.16mb.com/toppicks1.png -O toppicks1.png
-wget http://toppicks.16mb.com/toppicks2.png -O toppicks2.png
-wget http://toppicks.16mb.com/toppicks3.png -O toppicks3.png
-wget http://toppicks.16mb.com/toppicks4.png -O toppicks4.png
-wget http://toppicks.16mb.com/toppicks5.png -O toppicks5.png
-wget http://toppicks.16mb.com/toppicks6.png -O toppicks6.png
-wget http://toppicks.16mb.com/skystore.png -O skystore.png
-wget http://toppicks.16mb.com/skycinema.png -O skycinema.png
-wget http://toppicks.16mb.com/boxsets.png -O boxsets.png
+wget https://raw.githubusercontent.com/danmed/toppicks/master/toppicks1.png -O toppicks1.png
+wget https://raw.githubusercontent.com/danmed/toppicks/master/toppicks2.png -O toppicks2.png
+wget https://raw.githubusercontent.com/danmed/toppicks/master/toppicks3.png -O toppicks3.png
+wget https://raw.githubusercontent.com/danmed/toppicks/master/toppicks4.png -O toppicks4.png
+wget https://raw.githubusercontent.com/danmed/toppicks/master/toppicks5.png -O toppicks5.png
+wget https://raw.githubusercontent.com/danmed/toppicks/master/toppicks6.png -O toppicks6.png
+wget https://raw.githubusercontent.com/danmed/toppicks/master/skystore.png -O skystore.png
+wget https://raw.githubusercontent.com/danmed/toppicks/master/skycinema.png -O skycinema.png
+wget https://raw.githubusercontent.com/danmed/toppicks/master/boxsets.png -O boxsets.png
