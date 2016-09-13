dd568e99edecbeb9476729f0ca93f8407610cef4
danmed <danmed@gmail.com>
2016-09-13 11:54:18 +0100

    Update toppicks.sh

--- toppicks.sh
+++ toppicks.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/danmed/toppicks/master/toppicks1.png -O toppicks1.png
-wget https://raw.githubusercontent.com/danmed/toppicks/master/toppicks2.png -O toppicks2.png
-wget https://raw.githubusercontent.com/danmed/toppicks/master/toppicks3.png -O toppicks3.png
-wget https://raw.githubusercontent.com/danmed/toppicks/master/toppicks4.png -O toppicks4.png
-wget https://raw.githubusercontent.com/danmed/toppicks/master/toppicks5.png -O toppicks5.png
-wget https://raw.githubusercontent.com/danmed/toppicks/master/toppicks6.png -O toppicks6.png
-wget https://raw.githubusercontent.com/danmed/toppicks/master/skystore.png -O skystore.png
-wget https://raw.githubusercontent.com/danmed/toppicks/master/skycinema.png -O skycinema.png
-wget https://raw.githubusercontent.com/danmed/toppicks/master/boxsets.png -O boxsets.png
+wget https://raw.githubusercontent.com/danmed/toppicks/master/toppicks1.png -O /usr/share/enigma2/slyk-1-hd/skyicons/toppicks1.png
+wget https://raw.githubusercontent.com/danmed/toppicks/master/toppicks2.png -O /usr/share/enigma2/slyk-1-hd/skyicons/toppicks2.png
+wget https://raw.githubusercontent.com/danmed/toppicks/master/toppicks3.png -O /usr/share/enigma2/slyk-1-hd/skyicons/toppicks3.png
+wget https://raw.githubusercontent.com/danmed/toppicks/master/toppicks4.png -O /usr/share/enigma2/slyk-1-hd/skyicons/toppicks4.png
+wget https://raw.githubusercontent.com/danmed/toppicks/master/toppicks5.png -O /usr/share/enigma2/slyk-1-hd/skyicons/toppicks5.png
+wget https://raw.githubusercontent.com/danmed/toppicks/master/toppicks6.png -O /usr/share/enigma2/slyk-1-hd/skyicons/toppicks6.png
+wget https://raw.githubusercontent.com/danmed/toppicks/master/skystore.png -O /usr/share/enigma2/slyk-1-hd/skyicons/skystore.png
+wget https://raw.githubusercontent.com/danmed/toppicks/master/skycinema.png -O "/usr/share/enigma2/slyk-1-hd/skyicons/sky cinema.png"
+wget https://raw.githubusercontent.com/danmed/toppicks/master/boxsets.png -O /usr/share/enigma2/slyk-1-hd/skyicons/boxsets.png
