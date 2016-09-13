a24a0b4a1ab2615424189feed65efa090768142c
danmed <danmed@gmail.com>
2016-09-13 08:27:35 +0100

    Create toppicks.sh

---
+++ toppicks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wget http://toppicks.16mb.com/toppicks1.png -O toppicks1.png
+wget http://toppicks.16mb.com/toppicks2.png -O toppicks2.png
+wget http://toppicks.16mb.com/toppicks3.png -O toppicks3.png
+wget http://toppicks.16mb.com/toppicks4.png -O toppicks4.png
+wget http://toppicks.16mb.com/toppicks5.png -O toppicks5.png
+wget http://toppicks.16mb.com/toppicks6.png -O toppicks6.png
+wget http://toppicks.16mb.com/skystore.png -O skystore.png
+wget http://toppicks.16mb.com/skycinema.png -O skycinema.png
+wget http://toppicks.16mb.com/boxsets.png -O boxsets.png
