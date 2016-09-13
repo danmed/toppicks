77956347db4c6a0b96c19ba06b75e11a6da267cc
danmed <danmed@gmail.com>
2016-09-13 12:39:36 +0100

    Create toppicks.py

---
+++ toppicks.py
@@ -0,0 +1,14 @@
+import urllib
+import urllib2
+urllib.urlretrieve("https://raw.githubusercontent.com/danmed/toppicks/master/toppicks1.png", "/usr/share/enigma2/slyk-1-hd/skyicons/toppicks1.png")
+urllib.urlretrieve("https://raw.githubusercontent.com/danmed/toppicks/master/toppicks2.png", "/usr/share/enigma2/slyk-1-hd/skyicons/toppicks2.png")
+urllib.urlretrieve("https://raw.githubusercontent.com/danmed/toppicks/master/toppicks3.png", "/usr/share/enigma2/slyk-1-hd/skyicons/toppicks3.png")
+urllib.urlretrieve("https://raw.githubusercontent.com/danmed/toppicks/master/toppicks4.png", "/usr/share/enigma2/slyk-1-hd/skyicons/toppicks4.png")
+urllib.urlretrieve("https://raw.githubusercontent.com/danmed/toppicks/master/toppicks5.png", "/usr/share/enigma2/slyk-1-hd/skyicons/toppicks5.png")
+urllib.urlretrieve("https://raw.githubusercontent.com/danmed/toppicks/master/toppicks6.png", "/usr/share/enigma2/slyk-1-hd/skyicons/toppicks6.png")
+urllib.urlretrieve("https://raw.githubusercontent.com/danmed/toppicks/master/sky cinema.png", "/usr/share/enigma2/slyk-1-hd/skyicons/sky cinema.png")
+urllib.urlretrieve("https://raw.githubusercontent.com/danmed/toppicks/master/skystore.png", "/usr/share/enigma2/slyk-1-hd/skyicons/skystore.png")
+urllib.urlretrieve("https://raw.githubusercontent.com/danmed/toppicks/master/boxsets.png", "/usr/share/enigma2/slyk-1-hd/skyicons/boxsets.png")
+
+
+
