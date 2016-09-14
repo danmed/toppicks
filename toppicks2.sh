01e3a5abb9d5d7e5b514190300288b5bb2d5e7bb
danmed <danmed@gmail.com>
2016-09-14 21:01:30 +0100

    Update toppicks2.sh

--- toppicks2.sh
+++ toppicks2.sh
@@ -13,7 +13,7 @@ sed -i 's/.net/.net.rsz.io/g' links.txt
 linenum=$(( ( RANDOM % 40 )  + 1 ))
 link=$(sed -n ${linenum}p "links.txt")
 name=toppicks1.png
-resize="?w=383&h=244&mode=stretch&format=png"
+resize="?w=400&h=240&mode=stretch&format=png"
 wget $link$resize -O /usr/share/enigma2/slyk-1-hd/skyicons/$name
 
 #Pick random line in links.txt and download it
