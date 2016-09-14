8351c6a22167d5611f389a37090164d7e6475c49
danmed <danmed@gmail.com>
2016-09-14 21:05:48 +0100

    Update toppicks2.sh

--- toppicks2.sh
+++ toppicks2.sh
@@ -36,3 +36,10 @@ link=$(sed -n ${linenum}p "links.txt")
 name=toppicks6.png
 resize="?w=183&h=116&mode=stretch&format=png"
 wget $link$resize -O /usr/share/enigma2/slyk-1-hd/skyicons/$name
+
+#Pick random line in links.txt and download it
+linenum=$(( ( RANDOM % 40 )  + 1 ))
+link=$(sed -n ${linenum}p "links.txt")
+name=gmanpanthro.png
+resize="?w=183&h=116&mode=stretch&format=png"
+wget $link$resize -O /usr/share/enigma2/slyk-1-hd/skyicons/$name
