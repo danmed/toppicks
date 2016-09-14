0cfa62c78add70076a0f5c522b1f88d2f0d91914
danmed <danmed@gmail.com>
2016-09-14 17:27:44 +0100

    Update toppicks2.sh

--- toppicks2.sh
+++ toppicks2.sh
@@ -14,25 +14,25 @@ linenum=$(( ( RANDOM % 40 )  + 1 ))
 link=$(sed -n ${linenum}p "links.txt")
 name=toppicks1.png
 resize="?w=383&h=244&mode=stretch&format=png"
-wget $link$resize -O /usr/share/enigma2/slyk-1-hd/skyicons/$name$extension
+wget $link$resize -O /usr/share/enigma2/slyk-1-hd/skyicons/$name
 
 #Pick random line in links.txt and download it
 linenum=$(( ( RANDOM % 40 )  + 1 ))
 link=$(sed -n ${linenum}p "links.txt")
 name=toppicks3.png
 resize="?w=183&h=116&mode=stretch&format=png"
-wget $link$resize -O /usr/share/enigma2/slyk-1-hd/skyicons/$name$extension
+wget $link$resize -O /usr/share/enigma2/slyk-1-hd/skyicons/$name
 
 #Pick random line in links.txt and download it
 linenum=$(( ( RANDOM % 40 )  + 1 ))
 link=$(sed -n ${linenum}p "links.txt")
 name=toppicks4.png
 resize="?w=183&h=116&mode=stretch&format=png"
-wget $link$resize -O /usr/share/enigma2/slyk-1-hd/skyicons/$name$extension
+wget $link$resize -O /usr/share/enigma2/slyk-1-hd/skyicons/$name
 
 #Pick random line in links.txt and download it
 linenum=$(( ( RANDOM % 40 )  + 1 ))
 link=$(sed -n ${linenum}p "links.txt")
 name=toppicks6.png
 resize="?w=183&h=116&mode=stretch&format=png"
-wget $link$resize -O /usr/share/enigma2/slyk-1-hd/skyicons/$name$extension
+wget $link$resize -O /usr/share/enigma2/slyk-1-hd/skyicons/$name
