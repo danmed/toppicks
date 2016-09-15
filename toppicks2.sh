2e589e02cf0c9e7b31cd5e6bd46b7e8ece412364
danmed <danmed@gmail.com>
2016-09-15 16:14:34 +0100

    Update toppicks2.sh

--- toppicks2.sh
+++ toppicks2.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# IF YOU USE THIS, PLEASE USE YOUR OWN FREE CLOUDINARY.COM ACCOUNT AND REPLACE THE BELOW WITH YOUR USERNAME
-username=toppicks
 #Delete existing file if it exists
 rm htmlpage
 rm htmlpage2
@@ -23,55 +21,68 @@ cat htmlpage3 | grep -Eo "(http|https)://www.asset1.net/tv/pictures/movie/[a-zA-
 
 cat *.txt > all.txt
 
-#sed -i 's/.net/.net.rsz.io/g' all.txt
+sed -i 's/.net/.net.rsz.io/g' all.txt
+sed -i 's/https/http/g' all.txt
 #sed -i 's/.net/.net.rsz.io/g' movies.txt
-#sed -i 's/.com/.com.rsz.io/g' all.txt
+sed -i 's/.com/.com.rsz.io/g' all.txt
 sed -i 's/?downsize=320//g' all.txt
 
 #Pick random line in all.txt and download it
 linenum=$(( ( RANDOM % 50 )  + 1 ))
 link=$(sed -n ${linenum}p "all.txt")
 name=toppicks1.png
-url1=http://res.cloudinary.com/
-url2=/image/fetch/w_400,h_240,f_png/
-echo $url1$username$url2$link
-wget $url1$username$url2$link -O /usr/share/enigma2/slyk-1-hd/skyicons/$name
+resize="?w=400&h=240&mode=stretch&format=png"
+echo $link$resize
+wget $link$resize -O /usr/share/enigma2/slyk-1-hd/skyicons/$name --user-agent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 
 #Pick random line in all.txt and download it
 linenum=$(( ( RANDOM % 50 )  + 1 ))
 link=$(sed -n ${linenum}p "all.txt")
 name=toppicks3.png
-url1=http://res.cloudinary.com/
-url2=/image/fetch/w_183,h_116,f_png/
-echo $url1$username$url2$link
-wget $url1$username$url2$link -O /usr/share/enigma2/slyk-1-hd/skyicons/$name
+resize="?w=183&h=116&mode=stretch&format=png"
+echo $link$resize
+wget $link$resize -O /usr/share/enigma2/slyk-1-hd/skyicons/$name --user-agent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 
 #Pick random line in all.txt and download it
 linenum=$(( ( RANDOM % 50 )  + 1 ))
 link=$(sed -n ${linenum}p "all.txt")
 name=toppicks4.png
-url1=http://res.cloudinary.com/
-url2=/image/fetch/w_183,h_116,f_png/
-echo $url1$username$url2$link
-wget $url1$username$url2$link -O /usr/share/enigma2/slyk-1-hd/skyicons/$name
+resize="?w=183&h=116&mode=stretch&format=png"
+echo $link$resize
+wget $link$resize -O /usr/share/enigma2/slyk-1-hd/skyicons/$name --user-agent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 
 #Pick random line in all.txt and download it
 linenum=$(( ( RANDOM % 50 )  + 1 ))
 link=$(sed -n ${linenum}p "all.txt")
 name=toppicks6.png
-url1=http://res.cloudinary.com/
-url2=/image/fetch/w_183,h_116,f_png/
-echo $url1$username$url2$link
-wget $url1$username$url2$link -O /usr/share/enigma2/slyk-1-hd/skyicons/$name
+resize="?w=183&h=116&mode=stretch&format=png"
+echo $link$resize
+wget $link$resize -O /usr/share/enigma2/slyk-1-hd/skyicons/$name --user-agent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 
 #Pick random line in all.txt and download it
 linenum=$(( ( RANDOM % 50 )  + 1 ))
 link=$(sed -n ${linenum}p "all.txt")
 name=gmanpanthro.png
-url1=http://res.cloudinary.com/
-url2=/image/fetch/w_183,h_116,f_png/
-echo $url1$username$url2$link
-wget $url1$username$url2$link -O /usr/share/enigma2/slyk-1-hd/skyicons/$name
+resize="?w=183&h=116&mode=stretch&format=png"
+echo $link$resize
+wget $link$resize -O /usr/share/enigma2/slyk-1-hd/skyicons/$name --user-agent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
+
+#Pick random line in movies.txt and download it
+linenum=$(( ( RANDOM % 5 )  + 1 ))
+link=$(sed -n ${linenum}p "movies.txt")
+name=toppicks2.png
+resize="?w=200&h=240&mode=stretch&format=png"
+echo $link$resize
+wget $link$resize -O /usr/share/enigma2/slyk-1-hd/skyicons/$name --user-agent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
+
+#Pick random line in movies.txt and download it
+linenum=$(( ( RANDOM % 5 )  + 1 ))
+link=$(sed -n ${linenum}p "movies.txt")
+name=toppicks5.png
+resize="?w=200&h=240&mode=stretch&format=png"
+echo $link$resize
+wget $link$resize -O /usr/share/enigma2/slyk-1-hd/skyicons/$name --user-agent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
+
 
 #Pick random line in movies.txt and download it
 linenum=$(( ( RANDOM % 5 )  + 1 ))
